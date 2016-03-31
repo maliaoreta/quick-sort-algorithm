024eec234ab586422358cd753b33527add8adc70
maliaoreta <maliaoreta@gmail.com>
2016-03-30 17:27:15 -1000

    added second array with duplicate numbers and 0s, passes

--- public/js/tests/quick-sort.spec.js
+++ public/js/tests/quick-sort.spec.js
@@ -8,6 +8,8 @@ describe('quickSort', function () {
   it('should take an Array as a single parameter, sort it using a quick sort algorithm, and return the sorted array', function () {
 
     var testArr = [3, 1, 2, 6, 4, 5];
+    var testArr2 = [3, 12, 32, 4, 76, 2, 43, 0, 2, 1, 43];
     expect(quickSort(testArr)).to.deep.equal([1, 2, 3, 4, 5, 6]);
+    expect(quickSort(testArr2)).to.deep.equal([0, 1, 2, 2, 3, 4, 12, 32, 43, 43, 76])
   })
 })
